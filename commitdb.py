c6b7570e593846967a360aea80c75fcad5bec8d3
Joseph Kogut <joseph.kogut@gmail.com>
2015-10-05 13:34:28 -0700

    Update DB init methods

--- commitdb.py
+++ commitdb.py
@@ -1,48 +1,62 @@
 import os
 from ctypes import *
 
-os.environ['PATH'] = 'C:\CommitCRM\Server' + ';' + os.environ['PATH']
+class CommitDB:        
+        def __init__(self, appName = 'CommitAgent', CRMPath = r'C:\CommitCRM'):
+                self.CRMPath = CRMPath
+                self.serverPath = CRMPath + r'\Server'
+                self.DBPath = CRMPath + r'\Db'
+                self.DBPathUTF = create_string_buffer(bytes(self.DBPath, 'UTF-8')
+                self.appName = appName
 
-CmDBDLL = windll.LoadLibrary("c:\commitcrm\server\cmtdbeng.dll")
+                os.environ['PATH'] = self.serverPath + ';' + os.environ['PATH']
+                self.CmDBEngDll = windll.LoadLibrary(self.serverPath + r'\cmtdbeng.dll')
+                self.CmDBQryDll = windll.LoadLibrary(self.serverPath + r'\cmtdbqry.dll')
 
-class CommitDB:
-	def __init__(self, appName = 'CommitAgent', CRMPath = r'C:/CommitCRM/Db'):
-		self.CRMPath = CRMPath
-		self.appName = appName
+                self.status = c_int()
+                
+        def InitDbEngDll(self):
+                path = create_string_buffer(bytes(self.DBPath, 'UTF-8'))
+                self.CmDBEngDll.CmtInitDbEngDll(self.appName, path, byref(self.status))
 
-		self.status = c_int()
-		print(self.status)
-	
-		os.environ['PATH'] = CRMPath + ';' + os.environ['PATH']
-		
-	def InitDbQryDll(self):
-		print(CmDBDLL.CmtInitDbEngDll)
-		CmDBDLL.CmtInitDbEngDll(self.appName, self.CRMPath, byref(self.status)) 
-		
-	def GetQueryRecIds(self, xml_request_buff, xml_request_buff_len,
-		xml_response_data_buff, xml_response_data_buff_len, status):
-		pass
-	
-	def GetRecordDataByRecId(self, xml_request_buff, xml_request_buff_len,
-		xml_response_data_buff, xml_response_data_buff_len, status):
-		pass
-		
-	def CmtGetFieldAttributesByRecId(self, xml_request_buff,
-		xml_request_buff_len, xml_response_data_buff,
-		xml_response_data_buff_len, status):
-		pass
-		
-	def CmtTerminateDbQryDll(self):
-		pass
-		
-	def CmtGetDescriptionByCode(self, code, desc_size, desc):
-		pass
-		
-	def CmtGetDescriptionByStatus(self):
-		pass
-		
+        def InitDbQryDll(self):
+                path = create_string_buffer(bytes(self.DBPath, 'UTF-8'))
+                self.CmDBEngDll.CmtInitDbQryDll(self.appName, self.DBPath, byref(self.status))
+                                                
+        def InsUpdRec(self):
+                CmtInsUpdRec(self.appName, table_id, data_buff, map_buff, flag, tbd, rec_id_buff_size,
+                             error_codes_buff_size, err_msg_buff_size,rec_id_buff, err_codes_buff,
+                             err_msg_buff, status)
+
+        def GetQueryRecIds(self, xml_request_buff, xml_request_buff_len,
+                xml_response_data_buff, xml_response_data_buff_len, status):
+                pass
+        
+        def GetRecordDataByRecId(self, xml_request_buff, xml_request_buff_len,
+                xml_response_data_buff, xml_response_data_buff_len, status):
+                pass
+                
+        def GetFieldAttributesByRecId(self, xml_request_buff,
+                xml_request_buff_len, xml_response_data_buff,
+                xml_response_data_buff_len, status):
+                pass
+                
+        def TerminateDbEngDll(self):
+                self.CmDBEngDll.CmtTerminateDbEngDll()
+
+        def TerminateDbQryDll(self):
+                self.CmDBEngDll.CmtTerminateDbQryDll()
+                
+        def GetDescriptionByCode(self, code, desc_size, desc):
+                pass
+                
+        def GetDescriptionByStatus(self):
+                pass
+                
 if __name__ == '__main__':
-	db = CommitDB()
-	print(db.InitDbQryDll())
-	print(db.status)
-	
+        db = CommitDB()
+        db.InitDbEngDll()
+        print(db.status)
+        db.TerminateDbEngDll()
+        print(db.status)
+        

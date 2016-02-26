cb245485615313f836bf0f80ab862fc8a782968d
Joseph Kogut <joseph.kogut@gmail.com>
2016-02-26 11:14:29 -0800

    Add Event entity

--- pycommit/entities.py
+++ pycommit/entities.py
@@ -206,6 +206,35 @@ class Asset(CRMEntity):
         super().__init__(crm_proxy, recid, auto_populate)
         self.entity_type = self.types['Asset']
 
+class Event(CRMEntity):
+    db_fields = {
+        "RecordID": "FLDEVTRECID",
+        "EventType": "FLDEVTWRITETOID",
+        "EmployeeRecID": "FLDEVTWORKERID",
+        "PrivateUser": "FLDEVTPRIVATEID",
+        "Account": "FLDEVTCARDID",
+        "Contact": "FLDEVTCONTACTID",
+        "Document": "FLDEVTDOCID",
+        "Done": "FLDEVTDONE",
+        "Date": "FLDEVTEVENTDATE",
+        "Description": "FLDEVTFREETEXT",
+        "StartTime": "FLDEVTFROMTIME",
+        "EndTime": "FLDEVTTOTIME",
+        "LinkedEntity": "FLDEVTLINKRECID",
+        "Field1": "FLDEVTFAMILY",
+        "Field2": "FLDEVTACTION",
+        "Field3": "FLDEVTPLACE",
+        "Field4": "FLDEVTPLACE1",
+        "Field5": "FLDEVTPLACE2",
+        "CreatedByUser": "FLDEVTCREATEUSERID",
+        "UpdatedByUser": "FLDEVTUPDATEUSER",
+    }
+
+    def __init__(self, crm_proxy=None, recid=None, auto_populate=True):
+        self._recid_field = self.db_fields['RecordID']
+        super().__init__(crm_proxy, recid, auto_populate)
+        self.entity_type = self.types['Event']
+
 class Charge(CRMEntity):
     db_fields = {
         "RecordID": "FLDSLPRECID",

155945d64c975f385899f130ae0791c1c1f300e8
Joseph Kogut <joseph.kogut@gmail.com>
2016-05-03 10:42:56 -0700

    lowlevel: Remove unused function stub

--- pycommit/lowlevel.py
+++ pycommit/lowlevel.py
@@ -425,8 +425,5 @@ class DBInterface:
 
                 return bytes(buffer.value).strip()
                 
-        def get_desc_by_status(self):
-                pass
-
         def get_status(self):
                 return self.status.value
